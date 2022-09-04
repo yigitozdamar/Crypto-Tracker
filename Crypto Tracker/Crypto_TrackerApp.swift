bd4361638cadbf5248f5ff3d46d15891a96a9190
yigit <yigitozdamar@hotmail.com>
2022-09-04 13:06:29 +0300

    Initial Commit

---
+++ Crypto Tracker/Crypto_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_TrackerApp.swift
+//  Crypto Tracker
+//
+//  Created by Yigit Ozdamar on 4.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
