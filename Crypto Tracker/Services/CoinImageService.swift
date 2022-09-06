9269677b87667203f9ea1d010d2ae20d00437c66
yigit <yigitozdamar@hotmail.com>
2022-09-06 20:11:41 +0300

    Saving Images to the File Manager

--- Crypto Tracker/Services/CoinImageService.swift
+++ Crypto Tracker/Services/CoinImageService.swift
@@ -15,23 +15,37 @@ class CoinImageService {
     
     private var imageSubscription : AnyCancellable?
     private let coin : CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName : String
     
     init(coin:CoinModel) {
         self.coin = coin
-        getCoinImage(urlString: coin.image)
+        self.imageName = coin.id
+       getCoinImage()
     }
     
-    private func getCoinImage(urlString : String){
+    private func getCoinImage(){
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName){
+            image = savedImage
+        }else {
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage(){
         
         guard let url = URL(string:coin.image) else { return  }
         
         imageSubscription = NetworkingManager.download(url: url)
-            .tryMap({ data in
+            .tryMap({ (data) -> UIImage? in
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return  }
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
     
