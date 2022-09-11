21c327db8f938748996a2111891073eae04a436f
yigit <yigitozdamar@hotmail.com>
2022-09-11 21:08:52 +0300

    Create a custom line chart to display historical price data

--- Crypto Tracker/Core/Detail/View/DetailView.swift
+++ Crypto Tracker/Core/Detail/View/DetailView.swift
@@ -34,21 +34,29 @@ struct DetailView: View {
     
     var body: some View {
         ScrollView{
-            VStack(spacing:20){
-                Text("")
-                    .frame(height:150)
-                
-                overviewTitle
-                Divider()
-                overviewContent
-                additionalTitle
-                Divider()
-                additionalContent
-                
+            
+            VStack {
+                ChartView(coin: detailVm.coin)
+                    .padding(.vertical)
+                VStack(spacing:20){
+
+                    overviewTitle
+                    Divider()
+                    overviewContent
+                    additionalTitle
+                    Divider()
+                    additionalContent
+                    
+                }
+                .padding()
             }
-            .padding()
         }
         .navigationTitle(detailVm.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItems
+            }
+        }
     }
 }
 
@@ -61,6 +69,17 @@ struct DetailView_Previews: PreviewProvider {
 }
 
 extension DetailView {
+    
+    private var navigationBarTrailingItems : some View {
+        HStack {
+            Text(detailVm.coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(Color.theme.secondaryText)
+            CoinImageView(coin: detailVm.coin)
+                .frame(width: 25, height: 25)
+        }
+    }
+    
     private var overviewTitle : some View {
         Text("Overview")
             .font(.title)
